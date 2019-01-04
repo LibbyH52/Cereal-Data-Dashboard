7a7223241b99ddde86bfa729be43f4aa9cb152d8
Libby Henry <libbyh52@gmail.com>
2019-01-04 22:04:01 +0000

    changed navbar code to toggle menu as initially wouldn't open again after clicking link

--- static/scripts/nav.js
+++ static/scripts/nav.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
  $(".navbar-nav li a").click(function(event) {
-            if (!$(this).parent().hasClass('navbar-toggler'))
-                $(".navbar-nav li a").addClass("collapse");
+    //if (!$(this).parent().hasClass('navbar-toggler'))
+        $(".navbar-collapse").collapse("toggle")
  });
 });
